b63feabe62e17b8a3375ff32785fa8c814b53fad
lazowill <149547465+lazowill@users.noreply.github.com>
2023-11-09 15:10:57 -0500

    Update repositorycheck.py

--- repositorycheck.py
+++ repositorycheck.py
@@ -7,3 +7,23 @@ def repo():
 print("Murtaaz")
 print("LazoWill")
 print("Abhiram")
+
+
+Sorting Through File w Parsing - Will:
+
+inventory = parse_inventory_data('path_to_csv_file')
+print(inventory)
+
+Sort Inventory - Will:
+#to sort shoe inventory by size
+sorted_inventory_by_size = sort_inventory(inventory_list, 'size')
+print(sorted_inventory_by_size)
+
+#to sort shoe inventory by price
+sorted_inventory_by_price = sort_inventory(inventory_list, 'price')
+print(sorted_inventory_by_price)
+
+#to sort shoe inventory by availability
+sorted_inventory_by_availability = sort_inventory(inventory_list, 'availability')
+print(sorted_inventory_by_availability)
+
