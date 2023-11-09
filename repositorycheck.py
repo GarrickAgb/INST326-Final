e1aa22aba565b88cc2923394792db658561d6df3
lazowill <149547465+lazowill@users.noreply.github.com>
2023-11-09 15:26:39 -0500

    Update repositorycheck.py

--- repositorycheck.py
+++ repositorycheck.py
@@ -9,21 +9,32 @@ print("LazoWill")
 print("Abhiram")
 
 
-Sorting Through File w Parsing - Will:
+Sorting Through File w Parsing & Sorting Inventory - Will:
 
-inventory = parse_inventory_data('path_to_csv_file')
-print(inventory)
+def parse_inventory_data('file_path'):
+#reads and parses CSV shoe inventory file into a list of dictionaries 
+    with open(file_path, mode='r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        inventory_list = [dict(item) for item in reader]
+#converts numerical fields from strings to neccesary data types
+for item in inventory_list:
+    item['size'] = int(item['size'])
+    item['price'] = float(item['price'])
+return inventory_list
 
-Sort Inventory - Will:
-#to sort shoe inventory by size
+def sort_inventory(inventory_list, sort_key);
+#sorts inventory based on given sort key
+    return sorted(inventory_list, key=lambda x: x[sort_key])
+    
+inventory_list = parse_inventory_data('path_to_csv_file')
+
+#sorts the shoe inventory by different criteria: size, price, availability
 sorted_inventory_by_size = sort_inventory(inventory_list, 'size')
-print(sorted_inventory_by_size)
-
-#to sort shoe inventory by price
 sorted_inventory_by_price = sort_inventory(inventory_list, 'price')
-print(sorted_inventory_by_price)
-
-#to sort shoe inventory by availability
 sorted_inventory_by_availability = sort_inventory(inventory_list, 'availability')
+
+#print sorted inventory 
+print(sorted_inventory_by_size)
+print(sorted_inventory_by_price)
 print(sorted_inventory_by_availability)
 
