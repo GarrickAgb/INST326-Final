f5aeaff30854e56ef42f6690607132f2db32df9a
GarrickAgb <84434696+GarrickAgb@users.noreply.github.com>
2023-11-10 22:12:22 -0500

    Update repositorycheck.py

--- repositorycheck.py
+++ repositorycheck.py
@@ -1,12 +1,6 @@
 import sys
 import argparse
 import sklearn
-print "Hello World"
-def repo():
-    
-print("Murtaaz")
-print("LazoWill")
-print("Abhiram")
 
 displaying the shoe inventory data - Murtaaz
 with open('shoe_inventory.txt', 'r', encoding = 'utf-8') as file:
