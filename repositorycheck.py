8619303fb53738d9b5ce31f0a7577aadc1e1fce1
abhimet <103293511+abhimet@users.noreply.github.com>
2023-11-10 21:56:27 -0500

    Update repositorycheck.py

--- repositorycheck.py
+++ repositorycheck.py
@@ -72,29 +72,25 @@ class Shoe_Search:
 search = Shoe_Search(shoe_inventory)
 search.display_shoes()
 
-class ShoeInventory_Graphs
-#will hope to add a class containing methods where i can display a visualization of our inventory and sales through graphs using Pandas.
-
+class ShoeInventory_Graphs:
+#will- hope to add a class containing methods where i can display a visualization of our inventory and sales through graphs using Pandas.
 Sorting Through File w Parsing & Sorting Inventory - Will:
-
-def parse_inventory_data('file_path'):
-#reads and parses CSV shoe inventory file into a list of dictionaries 
-    with open(file_path, mode='r', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        inventory_list = [dict(item) for item in reader]
-#converts numerical fields from strings to neccesary data types
-for item in inventory_list:
-    item['size'] = int(item['size'])
-    item['price'] = float(item['price'])
-return inventory_list
+    def parse_inventory_data(file_path):
+        #reads and parses CSV shoe inventory file into a list of dictionaries 
+        with open(file_path, 'r', encoding='utf-8') as f:
+            reader = f.readlines()
+            inventory_list = [dict(item) for item in reader]
+    for item in inventory_list:
+        item['size'] = int(item['size'])
+        item['price'] = float(item['price'])
+    return inventory_list
 
 def sort_inventory(inventory_list, sort_key);
 #sorts inventory based on given sort key
     return sorted(inventory_list, key=lambda x: x[sort_key])
-    
-inventory_list = parse_inventory_data('path_to_csv_file')
+inventory_list = parse_inventory_data('shoe.txt')
 
-#sorts the shoe inventory by different criteria: size, price, availability
+#sorts the shoe inventory by size, price, availability
 sorted_inventory_by_size = sort_inventory(inventory_list, 'size')
 sorted_inventory_by_price = sort_inventory(inventory_list, 'price')
 sorted_inventory_by_availability = sort_inventory(inventory_list, 'availability')
